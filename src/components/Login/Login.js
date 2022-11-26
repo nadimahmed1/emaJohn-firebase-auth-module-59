9fa43c21b9aeae1a4ed92cc6b8e829b3d786b566
nadimahmed1 <nadimarafat26@gmail.com>
2022-11-26 14:55:01 +0600

    sign in with email and password impliment

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,21 +1,48 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 import './Login.css'
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const navigate = useNavigate();
+
+    const [signInWithEmailAndPassword, user] = useSignInWithEmailAndPassword(auth)
+
+    const handleEmailBlur = event => {
+        setEmail(event.target.value);
+
+    }
+
+    const handlePasswordBlur = event => {
+        setPassword(event.target.value)
+    }
+
+    const handleFormSubmit = event => {
+        event.preventDefault();
+        signInWithEmailAndPassword(email, password);
+        if (user) {
+            navigate('/shop')
+        }
+    }
+
     return (
         <div className='form-container'>
 
-            <form>
+            <form onSubmit={handleFormSubmit}>
                 <div>
                     <h2 className='form-title'>Login</h2>
                     <div className='input-group'>
                         <label htmlFor="email">Email</label>
-                        <input type="email" name="email" id="" />
+                        <input onBlur={handleEmailBlur} type="email" name="email" id="" />
                     </div>
                     <div className='input-group'>
                         <label htmlFor="Password">Password</label>
-                        <input type="Password" name="password" id="" />
+                        <input onBlur={handlePasswordBlur} type="Password" name="password" id="" />
                     </div>
                     <input className='form-submit' type="submit" value="Login" />
                 </div>
